516e6da19a3340ca3ca1a415e42737e6dd19a4a7
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-09-18 22:16:00 -0300

    Primeiros exercicios

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+print("Calculadora")
+
+valor1 = float(input("Informe o primeiro valor: "))
+valor2 = float(input("Informe o segundo valor: "))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+divisao = valor1 / valor2
+multiplicacao = valor1 * valor2
+
+print(f"A soma é {soma}")
+print(f"A subtração é {subtracao} ")
+print("A divisão é {}".format(divisao))
+print("A multiplicação é {}".format(multiplicacao))
---
+++ nome_sobrenome_idade.py
@@ -0,0 +1,6 @@
+print("Esse programa exibirá seu nome completo e sua idade")
+nome = input("Digite seu primeiro nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+idade = input("Digite sua idade: ")
+nome_completo_idade = nome + " " + sobrenome + ", " + idade + " anos!"
+print(nome_completo_idade)
